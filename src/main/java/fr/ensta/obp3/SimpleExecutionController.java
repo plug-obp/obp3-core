00b9ff9c8f6ba7eb521900f975d6e51c29505fdc
Ciprian TEODOROV <ciprian.teodorov@gmail.com>
2025-02-12 15:34:34 +0100

    a start pause resume controller

---
+++ src/main/java/fr/ensta/obp3/SimpleExecutionController.java
@@ -0,0 +1,34 @@
+package fr.ensta.obp3;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class SimpleExecutionController<R> {
+    IExecutable<R> executable;
+    boolean running = true;
+    private final Thread worker;
+    R result;
+
+    public SimpleExecutionController(IExecutable<R> executable) {
+        this.executable = executable;
+        worker = new Thread(() -> {
+            LockSupport.park();
+            R result = executable.run(() -> !running);
+        });
+    }
+
+    public void pause() {
+        LockSupport.park(worker);
+    }
+    public void resume() {
+        LockSupport.unpark(worker);
+    }
+
+    public void start() {
+        worker.start();
+        resume();
+    }
+    public void stop() {
+        running = false;
+        LockSupport.unpark(worker);
+    }
+}
