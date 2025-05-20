26ea47d75b0a98d9982b7eb59bc3685f859c7594
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2025-05-20 22:36:06 +0200

    some more comments and computeIfAbsent instead of putIfAbsent

--- src/main/java/obp3/fixer/Fixer.java
+++ src/main/java/obp3/fixer/Fixer.java
@@ -22,6 +22,8 @@ public class Fixer<S, T> implements Function<S, T>{
     /// At the end of a run, it is copied into the permanent table and cleared.
     Map<S, T> mTransient = new IdentityHashMap<>();
     /// Records the observers of a node (The nodes from which reachable)
+    /// NC: In the Pottier algorithm this is a graph, I just store the parents here.
+    /// NC: Homework: what are the consequences ?
     Map<S, List<S>> mParents = new IdentityHashMap<>();
 
     /// The workset is based on a Queue, but it could just as well be based on a
@@ -29,12 +31,14 @@ public class Fixer<S, T> implements Function<S, T>{
     ///    priority queue, provided a sensible way of assigning priorities could
     ///    be found.
     ///
-    /// A node in the workset has no successors. (It can have predecessors.)  In
-    ///    other words, a predecessor (an observer) of some node is never in the
-    ///    workset. Furthermore, a node never appears twice in the workset
+    /// A node in the workset has no successors.
+    /// NC: The idea is that when a node gets in the workset, it is like newly discovered - so we forget it's successors
+    ///    (It can have predecessors.)
+    ///    In other words, a predecessor (an observer) of some node is never in the workset.
+    ///    Furthermore, a node never appears twice in the workset.
     ///
-    /// When a variable broadcasts a signal, all of its predecessors (observers)
-    ///    receive the signal. Any variable that receives the signal loses all of its
+    /// When a variable broadcasts a signal, all of its predecessors (observers) receive the signal.
+    ///    Any variable that receives the signal loses all of its
     ///    successors (that is, it ceases to observe anything) and is inserted into
     ///    the workset. This preserves the above invariant.
     Queue<S> mWorkset = new LinkedList<>();
@@ -50,19 +54,24 @@ public class Fixer<S, T> implements Function<S, T>{
     /// Invocations of 'apply' trigger the fixed point computation.
     @Override
     public T apply(S node) {
+        // if the node is fixed already just return it.
         var property = mFixed.get(node);
         if (property != null) { return property; }
         assert inactive;
         inactive = false;
         ensureTransient(node);
+        // while we have work to do just do it,
+        // - starting either at the node that we just added, if the workset was empty and node was not already transient
+        // - or continue with the workset in the defined 'order' (FIFO here).
         while (!mWorkset.isEmpty()) {
             var current = mWorkset.poll();
             solve(current);
         }
 
+        // we are done for this node.
         // copies the transient table into the permanent table, and
-        //    empties the transient table. This allows all nodes to be reclaimed
-        //    by the garbage collector.
+        //    empties the transient table.
+        //    This allows all nodes to be reclaimed by the garbage collector.
         mFixed.putAll(mTransient);
         mTransient.clear();
         inactive = true;
@@ -70,7 +79,9 @@ public class Fixer<S, T> implements Function<S, T>{
     }
 
     void ensureTransient(S node) {
+        // if a node is already transient, just return.
         if (mTransient.containsKey(node)) { return; }
+        // create a transient node starting it at the bottom of the lattice.
         mTransient.put(node, lattice.bottom());
         mParents.put(node, new ArrayList<>());
         mWorkset.add(node);
@@ -79,11 +90,10 @@ public class Fixer<S, T> implements Function<S, T>{
         if (mFixed.get(current) != null) return;
         final List<S> currentChildren = new ArrayList<>();
 
-        // The flag [alive] is used to prevent the client from invoking [request]
-        //     after this interaction phase is over. In theory, this dynamic check seems
-        //      required in order to argue that [request] behaves like a pure function.
-        //      In practice, this check is not very useful: only a bizarre client would
-        //      store a [request] function and invoke it after it has become stale.
+        // The flag [alive] is used to prevent the client from invoking [request] after this interaction phase is over.
+        // In theory, this dynamic check seems required in order to argue that [request] behaves like a pure function.
+        // In practice, this check is not very useful: only a bizarre client would
+        // store a [request] function and invoke it after it has become stale.
         final boolean[] alive = {true};
         Function<S, T> requestFunction = (S node) -> {
             assert alive[0];
@@ -94,10 +104,10 @@ public class Fixer<S, T> implements Function<S, T>{
             return mTransient.get(node);
         };
 
-        var newProperty = function.apply(current, requestFunction); //current.accept(function, requestFunction);
+        var newProperty = function.apply(current, requestFunction);
         alive[0] = false;
         for (S child : currentChildren) {
-            mParents.putIfAbsent(child, new ArrayList<>()).add(current);
+            mParents.computeIfAbsent(child, _ -> new ArrayList<>()).add(current);
         }
 
         // If the updated value differs from the previous value, record
