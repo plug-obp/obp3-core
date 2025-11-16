7fe463432aadbc61d34f38644b0ea7c6af3b71ca
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2025-11-16 03:30:19 +0100

    fixed equality that is wrongly done in records with array

--- obp3-algos/src/main/java/obp3/unification/syntax/App.java
+++ obp3-algos/src/main/java/obp3/unification/syntax/App.java
@@ -13,4 +13,17 @@ public record App(String name, Term ... terms) implements Term {
         return name + (terms.length > 0 ? "(" + Arrays.stream(terms).map(Object::toString).reduce("", (a, b) -> a.isEmpty() ? b : a + ", " + b)+ ")" : "")
                 ;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        App app = (App) o;
+        return name.equals(app.name) && Arrays.equals(terms, app.terms);
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 * name.hashCode() + Arrays.hashCode(terms);
+    }
 }
